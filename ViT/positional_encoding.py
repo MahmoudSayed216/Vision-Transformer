ad75312b3110db396ac0f8d9ca3d96bfdfa2b0ba
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-03-26 14:33:10 +0200

    overall structure of ViT

--- ViT/positional_encoding.py
+++ ViT/positional_encoding.py
@@ -6,7 +6,7 @@ class PositionalEncoder(nn.Module):
     def __init__(self, embed_dim, n_patches):
         super().__init__()
         
-        self.pos_encoding = nn.Parameter(torch.randn(1, n_patches+1, embed_dim))
+        self.pos_encoding = nn.Parameter(torch.randn(1, n_patches, embed_dim))
 
 
     
--- ViT/vision_transformer.py
+++ ViT/vision_transformer.py
@@ -14,7 +14,37 @@ class VisionTransformer(nn.Module):
         super().__init__()
         self.embedding_dim = {"s": 512, "m":768, "l":768}[size]
         self.patch_dim = 16
+        self.n_pathces = (224**2)/(16**2)+1 # for the [CLS] token
+        
+        self.patch_encoder = nn.Linear(in_features=self.patch_dim**2, out_features=self.embedding_dim)
+        self.pos_encoder = PositionalEncoder(embed_dim=self.embedding_dim, n_patches=self.n_pathces+1)
 
-        self.pos_encoder = PositionalEncoder()
+
+        self.transformer_encoder = ViTEncoder()
+        self.classification_head = nn.Linear(in_features=self.embedding_dim, out_features=1000)
 
 
+
+    def forward(self, x):
+        pass
+
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        pass
+
+
+
+class ViTEncoder(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        pass
